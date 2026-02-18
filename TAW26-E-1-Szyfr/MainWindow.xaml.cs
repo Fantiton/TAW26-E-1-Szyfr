4814be2a70f5b5129291a8d699bdfba1fc3d26ac
Mango67 <4422807@TE-GDA-D-D21E03>
2026-02-18 14:24:44 +0100

    Działanie szyfru, bez wszystkich warunków

--- TAW26-E-1-Szyfr/MainWindow.xaml.cs
+++ TAW26-E-1-Szyfr/MainWindow.xaml.cs
@@ -20,5 +20,36 @@ namespace TAW26_E_1_Szyfr
         {
             InitializeComponent();
         }
+
+        private void EncryptButton_Click(Object sender, EventArgs e)
+        {
+            List<char> inputText = TextTextBox.Text.ToList();
+
+            int key;
+            if (!int.TryParse(KeyTextBox.Text, out key))
+            {
+                key = 0;
+            }
+
+            List<char> encryptedText = new List<char>();
+
+            foreach (char c in inputText)
+            {
+                char encryptedChar = c; 
+                if (Char.IsLower(c))
+                {
+                    encryptedChar = (char)(((c + key - 'a') % 26) + 'a');
+                }
+
+                encryptedText.Add(encryptedChar);
+            }
+
+            EncryptedTextTextBlock.Text = new string(encryptedText.ToArray());
+        }
+
+        private void SaveCipherButton_Click(Object sender, EventArgs e)
+        {
+            
+        }
     }
 }
