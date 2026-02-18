230e11a1eef5b54c4f0a01d347b05bfdf157ccbc
Mango67 <4422807@TE-GDA-D-D21E03>
2026-02-18 14:00:54 +0100

    Dodaj pliki projektów.

---
+++ TAW26-E-1-Szyfr/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TAW26_E_1_Szyfr
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
